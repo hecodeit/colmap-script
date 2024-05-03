a45a1225fb85c90dc920264854e6bed680867784
hecodeit <hecodeit@gmail.com>
2024-05-03 02:42:27 +0200

    first commit

---
+++ convert_minoru.sh
@@ -0,0 +1,6 @@
+python convert.py -s DJI_20240224_merged/
+cd DJI_20240224_merged/
+mv DJI_20240224_merged/images/ DJI_20240224_merged/zip/
+mv DJI_20240224_merged/sparse/ DJI_20240224_merged/zip/
+zip -r DJI_20240224_merged.zip DJI_20240224_merged/zip/
+aws s3 cp DJI_20240224_merged.zip s3://mirai.es/minoru_colmap/
